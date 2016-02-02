75450db2e9564349cf28530784740bcf553abe77
jaewon415 <jaewon415@gmail.com>
2016-02-03 00:30:46 +0900

    Update song.java

--- song.java
+++ song.java
@@ -10,14 +10,13 @@ public class Song {
       secondVerse();
       thirdVerse();
       fourthVerse();
- fifthVerse();sticker: anchormansuccess.gif
+      fifthVerse();
       sixthVerse();
    }
    
    //This method prints series of lines for the first-verse.
    public static void firstVerse() {
- System.out.println("I once wrote a program that wouldn't
-compile");
+ System.out.println("I once wrote a program that wouldn't compile");
       lastTwo();
    }
    
@@ -30,35 +29,29 @@ compile");
    
    //This method prints lines for the third-verse.
    public static void thirdVerse() {
- System.out.println("\"Parse error,\" cried the
-compiler");
+ System.out.println("\"Parse error,\" cried the compiler");
       System.out.println("Luckily I'm such a code baller.");
       same();
    }
    
    //This method prints lines for the fourth-verse.
    public static void fourthVerse() {
- System.out.println("Now the compiler wanted an
-identifier");
- System.out.println("And I thought the situation was getting
-dire.");
+      System.out.println("Now the compiler wanted an identifier");
+      System.out.println("And I thought the situation was getting dire.");
       same2();
    }
    
    //This method prints lines for the fifth-verse.
    public static void fifthVerse() {
- System.out.println("Java complained it expected an enum");
- System.out.println("Boy, these computers really are
-dumb!");
+      System.out.println("Java complained it expected an enum");
+      System.out.println("Boy, these computers really are dumb!");
       same1();
    }
    
    //This method prints lines for the fifth-verse.
    public static void sixthVerse() {
- System.out.println("The program was missing a curly bracket on
-the first line");
- System.out.println("Hopping that it would be the last error and
-would work fine");
+      System.out.println("The program was missing a curly bracket on the first line");
+      System.out.println("Hopping that it would be the last error and would work fine");
       same4();
    }
    
@@ -71,35 +64,30 @@ Scum,");
    
    //This method prints the total of five lines.
    public static void same2() {
- System.out.println("I added a main method with its String[]
-args,");
+      System.out.println("I added a main method with its String[] args,");
       same();
    }
    //This method prints total of four lines.
    public static void same() {
- System.out.println("I added a backslash to escape the
-quotes,");
+      System.out.println("I added a backslash to escape the quotes,");
       same3();
    }
    
    //This method prints total of three lines.
    public static void same3() {
- System.out.println("I added System.out.println(\"I <3
-coding\"),");
+      System.out.println("I added System.out.println(\"I <3 coding\"),");
       lastTwo();
    }
    
    //This method prints total of seven lines.
    public static void same4() {
- System.out.println("I added a curly bracket next to the name of
-program");
+      System.out.println("I added a curly bracket next to the name of program");
       same1();
    }
    
    //The Last two sentences that appear on every verse.
    public static void lastTwo() {
- System.out.println("I don't know why it wouldn't
-compile,");
+      System.out.println("I don't know why it wouldn't compile,");
       System.out.println("My TA just smiled.");
       System.out.println();
    }
